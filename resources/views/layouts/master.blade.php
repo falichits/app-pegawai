5bae2a5d8e9a3e63ae546c3ba8dd77885139782c
falichits <abdullahfalichits@gmail.com>
2025-11-26 04:39:04 +0700

    Finalisasi Tugas 5: Menambahkan dan menghubungkan CSS kustom

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Aplikasi Pegawai')</title>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    
+    
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
 </head>
 <body>
 
@@ -22,5 +26,7 @@
         <p>© {{ date('Y') }} - Aplikasi Pegawai Laravel 11</p>
     </footer>
 
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
